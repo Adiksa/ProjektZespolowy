d2f53a9eb3cbbb7b483d27a4741b6ff3c629cbce
Adrian Hajkowski <adrian.hajkowski@gmail.com>
2020-06-08 18:22:41 +0200

    Complaint correction fix.

--- ProjektZespolowy/Complaint.cs
+++ ProjektZespolowy/Complaint.cs
@@ -23,7 +23,7 @@ namespace ProjektZespolowy
 
         public bool Correct()
         {
-            if (description == null && description == "")
+            if (description == null || description == "")
                 return false;
             if (photo == null)
                 return false;
