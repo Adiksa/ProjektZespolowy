7f1d0858506251a6bd5c6c038ebc9880fbbb1a74
Adrian Hajkowski <adrian.hajkowski@gmail.com>
2020-06-08 12:57:49 +0200

    ProgressBar added to shop.

--- ProjektZespolowy/Fragments/Shop.cs
+++ ProjektZespolowy/Fragments/Shop.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -20,6 +20,7 @@ namespace ProjektZespolowy.Fragments
         private TextView nameTextView;
         private GridView gridView;
         private List<Promotion> promotions;
+        private ProgressBar progressBar;
         
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,31 +29,55 @@ namespace ProjektZespolowy.Fragments
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             view = inflater.Inflate(Resource.Layout.shopOffers, container, false);
-            FireBaseConnector connector = new FireBaseConnector();
-            promotions = connector.GetPromotions();
-            if(promotions != null)
-            {
-                ShopGridViewAdapter adapter = new ShopGridViewAdapter(this.Activity.BaseContext, promotions);
-                gridView = view.FindViewById<GridView>(Resource.Id.grid_view_image_text);
-                gridView.Adapter = adapter;
-                gridView.ItemClick += (s, e) =>
-                {
-                    Toast.MakeText(this.Activity.BaseContext, "GridView Item: " + promotions[e.Position].text, ToastLength.Short).Show();
-                };
-            }
             ComponentsLocalizer();
             ActionHooker();
+            
             return view;
+        }
+
+        public override async void OnResume()
+        {
+            PromotionLoad();
+            base.OnResume();
         }
         private void ComponentsLocalizer()
         {
             picImageView = view.FindViewById<ImageView>(Resource.Id.shopImageView);
             nameTextView = view.FindViewById<TextView>(Resource.Id.shopTextView);
+            progressBar = view.FindViewById<ProgressBar>(Resource.Id.ShopProgressBar);
         }
 
         private void ActionHooker()
         {
 
+        }
+
+        private async Task PromotionLoad()
+        {
+            progressBar.Visibility = ViewStates.Visible;
+            await Task.Run(() =>
+            {
+                try
+                {
+                    FireBaseConnector connector = new FireBaseConnector();
+                    promotions = connector.GetPromotions();
+                    if (promotions != null)
+                    {
+                        ShopGridViewAdapter adapter = new ShopGridViewAdapter(this.Activity.BaseContext, promotions);
+                        gridView = view.FindViewById<GridView>(Resource.Id.grid_view_image_text);
+                        this.Activity.RunOnUiThread(() => { gridView.Adapter = adapter; });
+                        gridView.ItemClick += (s, e) =>
+                        {
+                            Toast.MakeText(this.Activity.BaseContext, "GridView Item: " + promotions[e.Position].text, ToastLength.Short).Show();
+                        };
+                    }
+                }
+                catch
+                {
+
+                }
+            });
+            progressBar.Visibility = ViewStates.Invisible;
         }
     }
 }
